870156d5079430023c6de5140482205ccf47705a
Vanessa García <vanvanemily@gmail.com>
2021-03-14 03:07:31 -0600

    Remove input text from Header component.

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import HideableText from '../HideableText/HideableText';
 
 import Logo from "../Logo/Logo"
 import s from './Header.module.scss'
@@ -9,8 +8,6 @@ const header = () => (
         <div className={s.logo}>
             <Logo />
         </div>
-            <input type="text" name="search" placeholder=""/>
-            <HideableText text="Some text"/>
     </header>
 )
 
