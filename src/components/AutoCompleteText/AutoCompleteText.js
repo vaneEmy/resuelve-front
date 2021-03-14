1e5c2f73e6dcf7039096dbb4ca46c57dc5012302
Vanessa García <vanvanemily@gmail.com>
2021-03-14 03:06:43 -0600

    Create an autocomplete text component.

---
+++ src/components/AutoCompleteText/AutoCompleteText.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react'
+
+import s from './AutoCompleteText.module.scss'
+
+class AutoCompleteText extends Component{
+
+    render(){
+        return (
+            <>
+                <input type="text" value={this.props.text} name="search" placeholder="" onChange={(e) => this.props.onChangeText(e) }/>
+                {/* <ul>
+                    { this.props.items.map(item => <li>{item.title}</li>) }
+                </ul> */}
+                {
+                    this.props.items.length > 0 &&
+                    <ul>
+                        { this.props.items.map(item => <li onClick={() => this.props.onSelectSuggestion(item)}>{item.title}</li>) }
+                    </ul>
+                }
+               
+            </>
+        )
+    }
+}
+
+export default AutoCompleteText
