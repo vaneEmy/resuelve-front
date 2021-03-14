36d79bbb728264bec672f9bc36d84fb87cd7fa05
Vanessa García <vanvanemily@gmail.com>
2021-03-14 03:08:22 -0600

    Create functionality to filter fimls list.

--- src/App.js
+++ src/App.js
@@ -11,12 +11,14 @@ import movie1 from './assets/images/movie1.jpeg'
 import movie2 from './assets/images/movie2.jpeg'
 import movie3 from './assets/images/movie3.jpeg'
 import Films from './components/Films/Films';
+import AutoCompleteText from './components/AutoCompleteText/AutoCompleteText'
 
 function App() {
 
   const[filmsList, setFilmsList] = useState([])
-  // const[allFilms, setAllFilms] = useState([])
-
+  const[suggestions, setSuggestions] = useState([])
+  const[newFilmsList, setNewFilmsList] = useState([])
+  const[text, setText] = useState("")
 
   useEffect(() => {
     getFilmsData()
@@ -32,7 +34,7 @@ function App() {
               ...film,
             }
           })
-          // setAllFilms(filmsData)
+          setNewFilmsList(filmsData)
           setFilmsList(filmsData)
         }
       }
@@ -41,12 +43,49 @@ function App() {
     }
   }
 
+  const onChangeAutoCompleteText = (e) =>{
+    console.log(e.target.value)
+    const value = e.target.value.toLowerCase();
+    let suggestions = value.length === 0 ? [] : filmsList.filter(f => f.title.toLowerCase().includes(value))
+
+
+    // if(value.length > 0){
+    //   const regex = new RegExp(`^${value}`, 'i')
+    //   suggestions = filmsList.sort((a, b) =>{
+    //     if(a.title > b.title){
+    //       return -1
+    //     }
+    //     if(b.title > a.title){
+    //       return 1
+    //     }
+    //     return 0;
+    //   }).filter(v => regex.test(v.title))
+    //   console.log("----- SUGGESTIONS -----")
+    //   console.log(suggestions)
+    // }
+    setText(value)
+    setSuggestions(suggestions)
+  }
+
+  const suggestionSelected = (value) =>{
+    console.log("--- SELECCIONE UN DATO :d")
+    console.log(value)
+    setText(value.title)
+    setSuggestions([])
+  }
+  // const renderSuggestions = () => {
+  //   if(suggestions.length === 0){
+  //     return null
+  //   }
+  // }
+
   return (
     <div>
       <Header />
       <div>
         <Carousel images={[movie1, movie2, movie3]} />
       </div>
+      <AutoCompleteText items={suggestions} onChangeText={onChangeAutoCompleteText} onSelectSuggestion={suggestionSelected} text={text}/>
       <div className={s.films}>
         <Films films={filmsList} />
       </div>
